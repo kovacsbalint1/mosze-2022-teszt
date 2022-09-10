3321d12a8e3af090800543cc4b265a22078feea1
Kovács Bálint <balintkovacs1001@gmail.com>
2022-09-10 14:45:07 +0200

    toyrocky

--- main.cpp
+++ main.cpp
@@ -4,18 +4,18 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; // alsóvonás hiánya a változónévből
-    std::cout << '1-100 ertekek duplazasa' //hiányzik a sorvég 
-    for (int i = 0;) //hiányos ciklus
+    int *b = new int[N_ELEMENTS];
+    std::cout << '1-100 ertekek duplazasa' <<std::endl;
+    for (int i = 0; i <N_ELEMENTS; i++)
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) //hibás felső határ
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:" //eredmény nincs kiírva
+        std::cout << "Ertek:" << b[i] << std:endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; //változó nincs inicializálva 
+    int atlag = 0;
     for (int i = 0; i < N_ELEMENTS, i++)
     {
         atlag += b[i]
